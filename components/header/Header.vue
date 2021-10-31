76189502d85d31d72653c718b665e5904e84388e
ibnshayed <ibnshayed@gmail.com>
2021-10-31 18:11:20 +0600

    button link added

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -2,9 +2,14 @@
   <div class="h-16 bg-blue-900 text-white text-lg capitalize">
     <div class="container mx-auto py-4 flex justify-between">
       <div>
-        <h1 class="font-extrabold text-2xl tracking-wider">Vuewind</h1>
+        <h1
+          class="font-extrabold text-2xl tracking-wider cursor-pointer"
+          @click="$router.push('/')"
+        >
+          Vuewind
+        </h1>
       </div>
-      <div >
+      <div>
         <NuxtLink to="" class="hover:underline">Docs</NuxtLink>
       </div>
       <div class="space-x-4 divide-x-2">
